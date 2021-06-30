fa7aa0cc4e297c98950e334fe9ccf61e19f7a4f9
pratikgadhave <83935309+pratikgadhave@users.noreply.github.com>
2021-06-30 11:24:31 +0530

    Create footer.js

---
+++ src/footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Footer=()=>{
+  return(
+    
+    <div>
+      <h1>Made with 💖 by princepratikk<h1/>
+    
+    <div/>
+  );
+  
+  
+}
+
+export default Footer;
+
